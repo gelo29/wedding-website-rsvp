eda78cb6deafaa9c6fed08a8b1feb3485b8087bc
Angelo <g.samson.to@gmail.com>
2025-09-16 15:32:12 +0800

    DEBUG turned off

--- wedding_event/wedding_proj/settings.py
+++ wedding_event/wedding_proj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3(!=3#_t)vnkrp1#u^x243@&5qmh!6oo2s20t_+r*f$&=n*703'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
