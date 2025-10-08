f8dcda67d9c0ba092e727a1f28170d32badeae8b
Angelo <g.samson.to@gmail.com>
2025-10-08 12:31:11 +0800

    update start.sh

--- wedding_event/start.sh
+++ wedding_event/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn wedding_event.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn  wedding_proj.wsgi:application --bind 0.0.0.0:$PORT
