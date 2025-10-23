f4555bd02d8c15a65a3d1d17d12dedb0af18f196
Angelo <g.samson.to@gmail.com>
2025-10-23 11:16:52 +0800

    update admin guest count to display only if reply field is true

--- wedding_event/wedding_rsvp/admin.py
+++ wedding_event/wedding_rsvp/admin.py
@@ -3,6 +3,24 @@ from .models import Guest
 
 class WeddingAdmin(admin.ModelAdmin):
     list_display = ("first_name","last_name","nick_name","reply","message")
+    
+    def changelist_view(self, request, extra_context = None):
+        qs = self.get_queryset(request)
+        attending = qs.filter(reply=True).count()
+        total = qs.count()
+        
+        if total > 0:
+            pct = round((total/100) * 100)
+        else:
+            pct = 0
+        
+        extra_context = extra_context or {}
+        
+        extra_context["attending_count"] = attending
+        extra_context["total_count"] = total
+        extra_context["attendance_pct"] = pct
+        
+        return super().changelist_view(request, extra_context)
 
 admin.site.register(Guest, WeddingAdmin)
 admin.site.site_header = "Angelo & Celly Wedding Admin"
